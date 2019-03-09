4c8a4cadf41b8075a1f132ddece76221c195e00b
Vincent Cunningham <v.cunningham@flagrama.com>
2019-03-09 12:42:04 -0500

    Use gunicorn for functional test, add testing requirements

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,31 +1,39 @@
 import unittest
-from os import environ
+import subprocess
 
-from flask_testing import LiveServerTestCase
+from flask_testing import TestCase
 from selenium import webdriver
 
 from web.app import create_app
 
 
-class LoginTest(LiveServerTestCase):
+class LoginTest(TestCase):
 
     def create_app(self):
-        config_name = 'testing'
+        config_name = 'selenium'
         app = create_app(config_name)
         self.client = app.test_client()
         return app
 
     def setUp(self):
+        self.webserver = subprocess.Popen([
+            'gunicorn',
+            'web.wsgi:app',
+            '--bind=0.0.0.0:5000',
+            '--certfile=util/server.crt',
+            '--keyfile=util/server.key'
+        ])
         options = webdriver.ChromeOptions()
         options.add_argument('headless')
         self.driver = webdriver.Chrome(options=options)
 
     def tearDown(self):
         self.driver.quit()
+        self.webserver.terminate()
 
     def test_can_login(self):
         # The user goes to check out the Guessing Game Twitch bot
-        self.driver.get(self.get_server_url())
+        self.driver.get('https://localhost:5000')
 
         # He notices a button asking him to login using Twitch
         login_button = self.driver.find_element_by_id('login_with_twitch')
