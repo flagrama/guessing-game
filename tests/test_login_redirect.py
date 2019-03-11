bb5fab678c3be4d680c282322a1742ce07ba5ba1
Vincent Cunningham <v.cunningham@flagrama.com>
2019-03-11 11:13:39 -0400

    More complete tests, add flashed messages

--- tests/test_login_redirect.py
+++ tests/test_login_redirect.py
@@ -26,16 +26,31 @@ class LoginRedirectTest(TestCase):
             def text(self):
                 return self.text
 
+        if 'noauth' in args[0]:
+            return None
         if '/token' in args[0]:
             return MockResponse("""{"access_token": "abc123"}""", 200)
 
         return MockResponse(None, 404)
 
-    def test_redirects_to_home(self):
+    def test_login_button_navigates_to_twitch(self):
         response = self.client.get('/login')
         expected_location = 'https://id.twitch.tv/oauth2/authorize'
         actual_location = parse.urljoin(response.location, parse.urlparse(response.location).path)
         self.assertEqual(expected_location, actual_location)
+
+    def test_redirects_to_home(self):
+        response = self.client.get('/login/authorized')
+        self.assert_redirects(response, '/')
+
+    def test_twitch_csrf(self):
+        response = self.client.get('/login/authorized', follow_redirects=True)
+        self.assertTrue('Access denied' in str(response.data))
+
+    @mock.patch('requests.post', side_effect=mocked_requests_post)
+    def test_twitch_not_authorized(self, mock_post):
+        response = self.client.get(f'/login/authorized?code=noauth&state={app.secret_key}', follow_redirects=True)
+        self.assertTrue('Access denied' in str(response.data))
 
     @mock.patch('requests.post', side_effect=mocked_requests_post)
     def test_twitch_token_set(self, mock_post):
