117db8a08cde8128bccfafb1c0bd91a356a129e0
Vincent Cunningham <v.cunningham@flagrama.com>
2019-03-12 17:56:04 -0400

    Fix wrong URL for Twitch API

--- web/twitch.py
+++ web/twitch.py
@@ -5,12 +5,13 @@ from urllib.parse import quote
 import web.config as config
 
 
-twitch_base = "https://id.twitch.tv/oauth2"
+twitch_auth_base = "https://id.twitch.tv/oauth2"
+twitch_api_base = "https://api.twitch.tv/helix"
 scope = "user_read"
 
 
 def authorize(redirect_uri, state):
-    return (twitch_base + '/authorize'
+    return (twitch_auth_base + '/authorize'
             + f"?client_id={config.TWITCH_CLIENT_ID}"
             + f"&redirect_uri={redirect_uri}"
             + f"&response_type=code"
@@ -19,7 +20,7 @@ def authorize(redirect_uri, state):
 
 
 def create_token(authorization_code, redirect_uri):
-    response = requests.post(twitch_base + '/token'
+    response = requests.post(twitch_auth_base + '/token'
                              + f"?client_id={config.TWITCH_CLIENT_ID}"
                              + f"&client_secret={config.TWITCH_SECRET}"
                              + f"&code={authorization_code}"
@@ -31,20 +32,20 @@ def create_token(authorization_code, redirect_uri):
 
 
 def validate_token(token):
-    response = requests.get(twitch_base + '/validate', headers={'Authorization': f'OAuth {token}'})
+    response = requests.get(twitch_auth_base + '/validate', headers={'Authorization': f'OAuth {token}'})
     if response.status_code != 200:
         return None
     return json.loads(response.text)
 
 
 def revoke_token(token):
-    requests.post(twitch_base + '/revoke'
+    requests.post(twitch_auth_base + '/revoke'
                   + f"?client_id={config.TWITCH_CLIENT_ID}"
                   + f"&token={token}")
 
 
 def refresh_token(token_refresh_token):
-    response = requests.post(twitch_base + '/token'
+    response = requests.post(twitch_auth_base + '/token'
                              + f"?grant_type=refresh_token"
                              + f"&refresh_token={quote(token_refresh_token)}"
                              + f"&client_id={config.TWITCH_CLIENT_ID}"
@@ -62,7 +63,7 @@ def get_users(token, refresh, user_names, allow_refresh=True):
     # for user in user_names[1:]:           # If querying multiple users
     #     parameters += f'&login={user}'
     headers = {'Authorization': f'Bearer {token}'}
-    response = requests.get(twitch_base + '/users' + parameters, headers=headers)
+    response = requests.get(twitch_api_base + '/users' + parameters, headers=headers)
     if response is None:
         return None, None, None
     if response.status_code == 401 and 'www-authenticate' in response.headers and allow_refresh:
