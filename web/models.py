17ab90e9cd2a3f0cf0666b3bc3fefaa92a700f82
Vincent Cunningham <v.cunningham@flagrama.com>
2019-03-21 19:09:17 -0400

    Add Guessable model

--- web/models.py
+++ web/models.py
@@ -8,9 +8,9 @@ class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     twitch_id = db.Column(db.Integer)
-    twitch_login_name = db.Column(db.String())
-    twitch_display_name = db.Column(db.String())
-    bot_enabled = db.Column(db.Boolean())
+    twitch_login_name = db.Column(db.String)
+    twitch_display_name = db.Column(db.String)
+    bot_enabled = db.Column(db.Boolean, nullable=False)
 
     def __init__(self, twitch_id, twitch_login_name, twitch_display_name):
         self.twitch_id = twitch_id
@@ -39,3 +39,21 @@ class User(UserMixin, db.Model):
     @staticmethod
     def get_user_by_id(id):
         return User.query.filter_by(id=id).first()
+
+
+class Guessable(db.Model):
+    from sqlalchemy.dialects.postgresql import UUID
+    from uuid import uuid4
+
+    __tablename__ = 'guessables'
+
+    uuid = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    name = db.Column(db.String)
+    variations = db.Column(db.PickleType)
+
+    def __init__(self, name, variations):
+        self.name = name
+        self.varations = variations
+
+    def __repr__(self):
+        return f'<guessable uuid: {self.uuid}>'
