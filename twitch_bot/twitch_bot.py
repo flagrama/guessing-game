50bc4fe6aa8be5a4c4d16c816433c7b7e01445f3
Vincent Cunningham <v.cunningham@flagrama.com>
2019-03-25 10:59:44 -0400

    Move bot commands to own methods

--- twitch_bot/twitch_bot.py
+++ twitch_bot/twitch_bot.py
@@ -45,13 +45,13 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
     def do_command(self, event, command):
         user_id = self.get_tag(event.tags, 'user-id')
         room_id = self.get_tag(event.tags, 'room-id')
-        username = self.get_tag(event.tags, 'display-name').lower()
         is_mod = bool(int(self.get_tag(event.tags, 'mod')))
         active_games = self.redis_server.smembers('active_games')
         pending_games = self.redis_server.lrange('pending_games', 0, -1)
         is_active = bool([x for x in active_games if event.target.encode() == x])
         is_pending = bool([x for x in pending_games if event.target.encode() == x])
         args_list = event.arguments[0].lower().split(' ')
+
         if user_id == room_id or is_mod:
             if command == "start":
                 if not is_active and not is_pending:
@@ -67,41 +67,43 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
                 self.connection.privmsg(event.target, 'Guessing Game not yet active')
             if command == "answer":
                 if is_active and len(args_list) > 1:
-                    variations = [x for x in Database.execute_select_sql(
-                        Database.SQL_CURRENT_USER_VARIATIONS.format(room_id)
-                    )]
-                    result = self.check_variations(variations, args_list[1])
-                    if result:
-                        self.redis_server.publish(event.target, f'ANSWER {args_list[1]}')
+                    self.answer_command(room_id, args_list[1], event.target)
         if command == "points":
+            username = self.get_tag(event.tags, 'display-name').lower()
             if is_active:
-                data = Database.execute_select_sql(Database.SQL_GET_POINTS.format(
-                    'current_points', room_id, user_id
-                ))
-                if data:
-                    points = data[0][0]
-                    self.connection.privmsg(event.target, f"{username} has {points} points in the active game.")
+                self.points_command(user_id, room_id, event.target, username, 'current_points')
             else:
-                data = Database.execute_select_sql(Database.SQL_GET_POINTS.format(
-                    'points', room_id, user_id
-                ))
-                if data:
-                    points = data[0][0]
-                    self.connection.privmsg(event.target, f"{username} has {points} total points.")
+                self.points_command(user_id, room_id, event.target, username, 'points')
         if command == "guess":
             if is_active and len(args_list) > 1:
-                participant = Database.execute_select_sql(Database.SQL_GET_PARTICIPANT.format(room_id, user_id))
-                if not participant:
-                    channel_user_id = Database.execute_select_sql(Database.SQL_CHANNEL_USER_ID.format(room_id))[0][0]
-                    create_participant_sql = Database.SQL_PARTICIPANT_INSERT.format(
-                        uuid4(), username, user_id, channel_user_id
-                    )
-                    Database.execute_insert_update_sql(create_participant_sql)
-                variations = [x for x in Database.execute_select_sql(
-                    Database.SQL_CURRENT_USER_VARIATIONS.format(room_id))]
-                result = self.check_variations(variations, args_list[1])
-                if result:
-                    self.redis_server.publish(event.target, f'GUESS {user_id} {args_list[1]}')
+                username = self.get_tag(event.tags, 'display-name').lower()
+                self.guess_command(user_id, room_id, event.target, username, args_list[1])
+
+    def answer_command(self, room_id, variation, channel_name):
+        variations = [x for x in Database.execute_select_sql(Database.SQL_CURRENT_USER_VARIATIONS.format(room_id))]
+        result = self.check_variations(variations, variation)
+        if result:
+            self.redis_server.publish(channel_name, f'ANSWER {variation}')
+
+    def guess_command(self, user_id, room_id, channel_name, username, variation):
+        participant = Database.execute_select_sql(Database.SQL_GET_PARTICIPANT.format(room_id, user_id))
+        if not participant:
+            channel_user_id = Database.execute_select_sql(Database.SQL_CHANNEL_USER_ID.format(room_id))[0][0]
+            create_participant_sql = Database.SQL_PARTICIPANT_INSERT.format(
+                uuid4(), username, user_id, channel_user_id
+            )
+            Database.execute_insert_update_sql(create_participant_sql)
+        variations = [x for x in Database.execute_select_sql(
+            Database.SQL_CURRENT_USER_VARIATIONS.format(room_id))]
+        result = self.check_variations(variations, variation)
+        if result:
+            self.redis_server.publish(channel_name, f'GUESS {user_id} {variation}')
+
+    def points_command(self, user_id, room_id, channel_name, username, type):
+        data = Database.execute_select_sql(Database.SQL_GET_POINTS.format(f"{type}", room_id, user_id))
+        if data:
+            points = data[0][0]
+            self.connection.privmsg(channel_name, f"{username} has {points} points in the active game.")
 
     def handle_messages(self, message):
         message = message.decode('utf-8')
@@ -121,7 +123,9 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
     @staticmethod
     def check_variations(variations, user_variation):
         return [
-            item for sublist in variations for subsublist in sublist for item in subsublist if item == user_variation
+            item for sublist in variations
+            for subsublist in sublist
+            for item in subsublist if item == user_variation
         ]
 
     @staticmethod
